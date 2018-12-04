445132e682c5a26f07495450f83883a575f07fab
emgie <macgryczka20@gmail.com>
2018-12-04 22:39:20 +0100

    changes in api
    remove simple string in response, add httpStatus

--- infrastructures/src/main/java/pl/emgie/ocaexamquestionsapp/api/QuestionRestController.java
+++ infrastructures/src/main/java/pl/emgie/ocaexamquestionsapp/api/QuestionRestController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.emgie.ocaexamquestionsapp.questions.QuestionService;
@@ -30,12 +31,12 @@ class QuestionRestController {
     @DeleteMapping(value="/del/", params = {"questionId"})
     ResponseEntity<?> delete(@Param("questionId") String questionId ) {
         questionService.delete(questionId);
-        return ResponseEntity.ok("Ok");
+        return ResponseEntity.ok(HttpStatus.OK);
     }
 
     @PostMapping("/")
     ResponseEntity<?> save(@RequestBody QuestionDto dto) {
         questionService.save(dto);
-        return ResponseEntity.ok("Ok");
+        return ResponseEntity.ok(HttpStatus.OK);
     }
 }
